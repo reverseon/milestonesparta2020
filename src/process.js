cfb90371b2ef723d71c91f7d1205170e3761dcee
reverseon <reverseon@outlook.com>
2021-08-01 15:02:07 +0700

    Minor add

--- src/process.js
+++ src/process.js
@@ -1,5 +1,14 @@
-const navbar = document.querySelector('.navbar-nav')
-
+const navbar = document.querySelector('.navbar-nav');
+const nvcolor = document.querySelector('.navbar');
+const maxalphnav = 0.7;
+if (scrollY !== 0) {
+    let aval = scrollY/640 * maxalphnav;
+    nvcolor.setAttribute('style', `background: rgba(0,0,0,${aval < maxalphnav ? aval : maxalphnav});`)
+}
+const furl = location.href;
+const target = `#${furl.split('#')[1]}nav`
+const factive = document.querySelector(target);
+factive.parentElement.classList.add('activelink');
 navbar.addEventListener('click', e => {
     console.log(e.target.tagName);
     console.log(e.target.classList);
@@ -13,3 +22,12 @@ navbar.addEventListener('click', e => {
         e.target.parentElement.classList.add('activelink');
     }
 })
+
+window.addEventListener('scroll', () => {
+    if (scrollY !== 0) {
+        let aval = scrollY/640 * maxalphnav;
+        nvcolor.setAttribute('style', `background: rgba(0,0,0,${aval < maxalphnav ? aval : maxalphnav});`)
+    } else {
+        nvcolor.setAttribute('style', 'background: rgba(0,0,0,0);')
+    }
+});
